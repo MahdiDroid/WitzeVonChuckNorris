a651d3696db9dc23df7fdc9664b0584cb1e4a0b5
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-03 10:44:45 +0300

    Initial commit

---
+++ src/test/java/com/mahdidroid/witze/WitzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahdidroid.witze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
