cc366d9cc2f5c5ac175b2591f0231a38f78a3bb3
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-03 11:54:23 +0300

    adding  configuration

---
+++ src/main/java/com/mahdidroid/witze/config/chuckConfiguration.java
@@ -0,0 +1,16 @@
+package com.mahdidroid.witze.config;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class chuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
