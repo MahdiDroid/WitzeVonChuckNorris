2b1dba92a6bcd5be5b8b5a19a92da1d59f607f2a
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-04 11:39:35 +0300

    chuck-config xml added but not used

--- src/main/java/com/mahdidroid/witze/services/JokeServiceImpl.java
+++ src/main/java/com/mahdidroid/witze/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mahdidroid.witze.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -13,7 +14,7 @@ public class JokeServiceImpl implements JokeService {
 //        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
 //    }
 
-
+    @Autowired
     public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
         this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
